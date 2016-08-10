26a66ceed3cf249b2b372eefac0006f34d187472
Timothy Craig <ttcraig@Timothys-MacBook-Pro.local>
2016-08-09 22:58:31 -0400

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Redux Material-UI Starter
+
+```
+$ npm install
+$ npm start
+$ open http://localhost:3000/
+```
---
+++ src/app/components/app.js
@@ -0,0 +1,42 @@
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// // needed for onTouchTap
+// // http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+import React, { Component } from 'react';
+import {deepOrange500} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import DatePickerExampleSimple from './DatePickerExampleSimple';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+
+const styles = {
+  appBar: {
+    display: 'flex'
+  }
+}
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          <AppBar title="My App" style={styles.appBar} />
+          <DatePickerExampleSimple />
+
+          <TextField
+            hintText="Hint Text"
+            floatingLabelText="Floating Label Text"
+          />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
